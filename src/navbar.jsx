27ec6e67d197a090cc885c2254f3331b7fd0ce10
Sajid ikbal <sajid402689@gmail.com>
2025-11-09 02:05:06 +0530

    fix: corrected Navbar.css import path

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./Navbar.css";
+import "./navbar.css";
 
 function Navbar({ onNavClick, activeSection }) {
   const [menuOpen, setMenuOpen] = useState(false);
